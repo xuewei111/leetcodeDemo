451b5194af2abc0dc1fa91f4a409d7c3cf0020a0
xuewei111 <m13358109269_1@163.com>
2023-02-06 23:16:11 +0800

    二分法-二分查找

---
+++ src/main/java/com/二分查找/main.java
@@ -0,0 +1,38 @@
+package main.java.com.二分查找;
+
+public class main {
+
+
+    public static void main(String[] args) {
+        System.out.println(search(new int[]{-1,0,3,5,9,12},9));
+    }
+
+
+    /**
+     * 二分法
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int search(int [] nums,int target) {
+//        for (int i = 0; i < nums.length; i++) {
+//            if(nums[i] == target) return i;
+//        }
+//        return -1;
+
+        int left = 0,right = nums.length - 1;
+        while (left <= right) {
+            int middle = left + ((right - left) / 2);
+            if (nums[middle] == target) {
+                return middle;
+            } else if (nums[middle] < target) {
+                left = middle + 1;
+            } else if (nums[middle] > target) {
+                right = middle -1;
+            }
+        }
+        return -1;
+    }
+}
+
+
